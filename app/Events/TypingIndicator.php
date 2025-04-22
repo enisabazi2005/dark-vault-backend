0543812c9d01628785438fbe9052c013e35653f8
enisabazi <enisabazi415work@gmail.com>
2025-04-22 18:09:20 +0200

    wip/implementing-message-seen-event-3

--- app/Events/TypingIndicator.php
+++ app/Events/TypingIndicator.php
@@ -9,40 +9,41 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 
 class TypingIndicator implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    public $senderId;
-    public $receiverId;
-    public $isTyping;
+    public $sender_id;
+    public $receiver_id;
+    public $is_typing;
 
     /**
      * Create a new event instance.
      */
-    public function __construct($senderId , $receiverId , $isTyping)
+    public function __construct($sender_id , $receiver_id, $is_typing)
     {
-        $this->senderId = $senderId;
-        $this->receiverId = $receiverId;
-        $this->isTyping = $isTyping;
+        $this->sender_id = $sender_id;
+        $this->receiver_id = $receiver_id;
+        $this->is_typing = $is_typing;
     }
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
-     */
     public function broadcastOn()
     {
-        return new Channel('typing-indicator.' . $this->receiverId);
+        return new Channel('chatroom.' . $this->receiver_id);
     }
 
     public function broadcastWith()
     {
         return [
-            'sender_id' => $this->senderId,
-            'is_typing' => $this->isTyping
+            'sender_id' => $this->sender_id,
+            'is_typing' => $this->is_typing,
         ];
+    }
+
+    public function broadcastAs()
+    {
+        return 'user.typing';
     }
 }
